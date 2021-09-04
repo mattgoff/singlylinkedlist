48ee2a830ea8298f7c9489dda6ea4593e448addc
Matt Goff <matt@goff.us>
2021-09-04 15:44:01 -0700

    more work

--- app.py
+++ app.py
@@ -43,6 +43,45 @@ class SinglyLinkedList:
     new_node.next = prev_node.next
     prev_node.next = new_node
 
+  def DeleteNode(self, key):
+    cur_node = self.head
+
+    if cur_node and cur_node.data == key:
+      self.head = cur_node.next
+      cur_node = None
+      return
+
+    prev_node = None
+    while cur_node and cur_node.data != key:
+      prev_node = cur_node
+      cur_node = cur_node.next
+
+    if cur_node is None:
+      return
+
+    prev_node.next = cur_node.next
+    cur_node = None
+
+  def DeleteNodeAtPosition(self, pos):
+    cur_node = self.head
+    if pos == 0:
+      self.head = cur_node.next
+      cur_node = None
+      return
+
+    prev_node = None
+    count = 1
+    while cur_node and count != pos:
+      prev = cur_node
+      cur_node = cur_node.next
+      count += 1
+
+    if cur_node is None:
+      return
+
+    prev_node = cur_node.next
+    cur_node = None
+
   def ReverseIterative(self):
     prev = None
     cur = self.head
@@ -56,7 +95,6 @@ class SinglyLinkedList:
     self.head = prev
 
   def ReverseRecursive(self):
-
     def _reverse_recursive(cur, prev):
       if not cur:
         return prev
@@ -69,6 +107,19 @@ class SinglyLinkedList:
 
     self.head = _reverse_recursive(cur=self.head, prev=None)
 
+  def GetLengthIterative(self):
+    cur_node = self.head
+    count = 0
+
+    while cur_node:
+      count += 1
+      cur_node = cur_node.next
+
+    return count
+
+  def GetLengthRecursive(self, node):
+    if node is None: return 0
+    return 1 + self.GetLengthRecursive(node.next)
 
 llist = SinglyLinkedList()
 llist.AppendNode("A")
@@ -77,6 +128,8 @@ llist.AppendNode("C")
 llist.AppendNode("D")
 llist.print_list()
 # llist.ReverseIterative()
-llist.ReverseRecursive()
+# llist.ReverseRecursive()
+llist.DeleteNode("C")
 llist.print_list()
-
+print("Itterative Length of list: {}".format(llist.GetLengthIterative()))
+print("Recursive Length of list: {}".format(llist.GetLengthRecursive(llist.head)))
